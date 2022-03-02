2e105899af06822254882664d81e347290b627fe
Carlos <carlospelaez@outlook.com>
2022-03-02 18:38:42 -0500

    Add link detail view from home

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 from .models import Post
 
@@ -7,3 +7,9 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()) #Existe la fecha de publicación
     stuff_for_frontend={'posts': posts}
     return render(request, 'blog/post_list.html', stuff_for_frontend)
+
+def post_detail(request, pk):
+    post = get_object_or_404 (Post, pk=pk)
+    stuff_for_frontend = {'post': post}
+    return render(request, 'blog/post_detail.html', stuff_for_frontend)
+
